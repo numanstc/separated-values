a58a6dd5d1f7ea11cff262f0c67c0f5a386a04d0
Numan Satıcı <numan_satici@hotmail.com>
2023-08-14 20:41:56 +0300

    empty lines tests added

--- src/test/java/com/numanstc/domain/FixedSizeValuesTest.java
+++ src/test/java/com/numanstc/domain/FixedSizeValuesTest.java
@@ -65,5 +65,41 @@ class FixedSizeValuesTest {
                 sut.getSeparatedValues()
         );
     }
+    @Test
+    void itShouldReturnThreeLinesAndTwoColumnsAndFirsLineNull() {
+        FixedSizeValues sut = new FixedSizeValues(2, 2);
+        sut.setSeparator(";");
+
+        sut.put(Point.create(2, 1), "Row 2, Col 1");
+        sut.put(Point.create(2, 2), "Row 2, Col 2");
+
+        assertEquals(
+                """
+                        ;
+                        Row 2, Col 1;Row 2, Col 2""",
+                sut.getSeparatedValues()
+        );
+    }
+
+    @Test
+    void itShouldReturnThreeLinesAndTwoColumnsAndLastLineNull() {
+        FixedSizeValues sut = new FixedSizeValues(3, 2);
+        sut.setSeparator(";");
+
+        sut.put(Point.create(1, 1), "Row 1, Col 1");
+        sut.put(Point.create(1, 2), "Row 1, Col 2");
+        sut.put(Point.create(2, 1), "Row 2, Col 1");
+        sut.put(Point.create(2, 2), "Row 2, Col 2");
+
+        assertEquals(
+                """
+                        Row 1, Col 1;Row 1, Col 2
+                        Row 2, Col 1;Row 2, Col 2
+                        ;""",
+                sut.getSeparatedValues()
+        );
+    }
+
+
 
 }
